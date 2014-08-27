d60bab880892f5a7990b1920f93f7ca1ea03dae1
Rika-chan <1slave25@gmail.com>
2014-08-27 21:18:46 +0400

    FIXING PERMISSIONS

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	DEBUG: false,
+	REDIS_PORT: 6379,
+	LISTEN_PORT: 8000,
+	TRUST_X_FORWARDED_FOR: false,
+	SOCKJS_PREFIX: '/sockjs',
+	SOCKJS_URL: 'http://desu.nipah.moe:8000/sockjs',
+	SOCKJS_SCRIPT_URL: 'http://desu.nipah.moe:8000/sockjs-0.3.2.min.js',
+	GAME_ID: 1,
+};
