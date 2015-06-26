cbe185c7bfb5419a9b59f01a2f7ea148b921ddb7
Rika-chan <Rika-chan@github.com>
2015-06-26 22:31:05 +0200

    Changing servers

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
 	LISTEN_PORT: 8000,
 	TRUST_X_FORWARDED_FOR: false,
 	SOCKJS_PREFIX: '/sockjs',
-	SOCKJS_URL: 'http://desu.nipah.moe:8000/sockjs',
-	SOCKJS_SCRIPT_URL: 'http://desu.nipah.moe:8000/sockjs-0.3.2.min.js',
+	SOCKJS_URL: 'http://miya.nipah.moe:8000/sockjs',
+	SOCKJS_SCRIPT_URL: 'http://miya.nipah.moe:8000/sockjs-0.3.2.min.js',
 	GAME_ID: 1,
 };
